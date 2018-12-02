26e7eee1c2ccb6979706f58a731bed2d338478c0
zachary <xs.zachary.zk@gmail.com>
2018-12-02 10:26:46 +0800

    轮播区域完成

---
+++ public/front/js/index.js
@@ -0,0 +1,21 @@
+//区域滚动
+; (function () {
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+        indicators: false, //是否显示滚动条
+        // scrollY: true, //是否竖向滚动
+        // scrollX: false, //是否横向滚动
+        // startX: 0, //初始化时滚动至x
+        // startY: 0, //初始化时滚动至y
+        // bounce: true //是否启用回弹  
+    });
+}())
+
+    // 轮播
+    ; (function () {
+        //获得slider插件对象
+        var gallery = mui('.mui-slider');
+        gallery.slider({
+            interval: 3000//自动轮播周期，若为0则不自动播放，默认为0；
+        });
+    }())
